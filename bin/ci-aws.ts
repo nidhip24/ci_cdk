f5b196ca3b53901a805e55013173ca99fa428a2d
nidhip24 <nidhipkathiriya@gmail.com>
2025-03-18 19:05:32 -0400

    init

--- bin/ci-aws.ts
+++ bin/ci-aws.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CiAwsStack } from '../lib/ci-aws-stack';
+import { MyCdkProjectStack } from '../lib/ci-aws-stack';
 
 const app = new cdk.App();
-new CiAwsStack(app, 'CiAwsStack', {
+new MyCdkProjectStack(app, 'CiAwsStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/ci-aws-stack.ts
+++ lib/ci-aws-stack.ts
@@ -1,16 +1,37 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
-export class CiAwsStack extends cdk.Stack {
+export class MyCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    // Create S3 bucket
+    const myBucket = new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,  // Only for dev/test environments
+    });
 
-    // The code that defines your stack goes here
+    const myLambda = new lambda.Function(this, 'MyLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          console.log('Lambda invoked!');
+          return { statusCode: 200, body: 'Hello, World!' };
+        }
+      `),
+      environment: {
+        BUCKET_NAME: myBucket.bucketName,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CiAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myTable = new dynamodb.Table(this, 'MyTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'MyTable',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,  // Only for dev/test environments
+    });
   }
 }
